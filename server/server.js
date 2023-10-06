a6d47677936ff05f7174683aff97e3a069a96a0c
kishorevenai <kishorevenai@gmail.com>
2023-10-06 23:47:15 +0530

    add

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 
-console.log('testing')
+console.log('testing main')
 
 const PORT = process.env.PORT || 3000
 
