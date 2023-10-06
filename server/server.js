4dd1877c0c69b5a01532afb5ac023fe41d608272
kishorevenai <kishorevenai@gmail.com>
2023-10-06 23:30:50 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 
-console.log('testing1')
+console.log('testing correct')
 
 const PORT = process.env.PORT || 3000
 
